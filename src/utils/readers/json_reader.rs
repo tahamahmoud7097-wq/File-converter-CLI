027f1432c1e45aedcbf203d89114e2dddde0fc67
tahamahmoud7097-wq <tahamahmoud7097@gmail.com>
2025-10-31 21:26:11 +0200

    Fixed JSON -> TOML conversion

--- src/utils/readers/json_reader.rs
+++ src/utils/readers/json_reader.rs
@@ -5,7 +5,9 @@ use toml::Value as toml_val;
 pub fn json_reader(path: &str) -> UniversalData {
     let content = std::fs::read_to_string(path).unwrap();
     let json: json_val = serde_json::from_str(&content).unwrap();
-    let toml_safe = toml_val::try_from(json)
-        .unwrap_or_else(|_| toml_val::String("unsupported value".to_string()));
+    let toml_safe = match toml_val::try_from(json) {
+        Ok(v) => v,
+        Err(_) => toml_val::String("Unsupported Value".to_string()),
+    };
     UniversalData::StructToml(toml_safe)
 }
--- src/utils/writers/toml_writer.rs
+++ src/utils/writers/toml_writer.rs
@@ -2,7 +2,19 @@ use crate::utilities::UniversalData;
 use toml::Value as toml_val;
 pub fn toml_writer(data: &UniversalData, path: &str) {
     if let UniversalData::StructToml(obj) = data {
-        std::fs::write(path, toml::to_string_pretty(obj).unwrap()).unwrap();
+        if let toml_val::Array(arr) = obj {
+            let mut output: String = String::new();
+            for item in arr {
+                if let toml_val::Table(tbl) = item {
+                    output.push_str("[[items]]\n");
+                    output.push_str(&toml::to_string_pretty(&tbl).unwrap());
+                    output.push('\n');
+                }
+            }
+            std::fs::write(path, output).unwrap();
+        } else {
+            std::fs::write(path, toml::to_string_pretty(obj).unwrap()).unwrap();
+        }
     } else if let UniversalData::Table { headers, rows } = data {
         let toml_arr: Vec<toml_val> = rows
             .iter()
